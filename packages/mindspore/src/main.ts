c2b5172bdf9d9e7d49c08d6b6f2821f851d343c1
ailoooong <ilong8@qq.com>
2023-12-25 11:38:34 +0800

    fix：删除不必要的文件

--- packages/mindspore/src/main.ts
+++ packages/mindspore/src/main.ts
@@ -1,13 +1,11 @@
 import { createApp } from 'vue';
 import VueDOMPurifyHTML from 'vue-dompurify-html';
 import 'amfe-flexible';
-import { router } from './routers';
 
 import 'shared/styles/index.scss';
 import App from './App.vue';
 
 const app = createApp(App);
 
-app.use(router);
 app.use(VueDOMPurifyHTML);
 app.mount('#app');
