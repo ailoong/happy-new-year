b8e6f9525891d13e4a2421c4be5a992e94f9130a
ailoooong <ilong8@qq.com>
2023-12-25 11:39:48 +0800

    fix：文件修改

--- packages/euler/src/main.ts
+++ packages/euler/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import VueDOMPurifyHTML from 'vue-dompurify-html';
 import 'amfe-flexible';
 import { router } from './routers';
+import 'shared/styles/index.scss';
 import App from './App.vue';
 
 const app = createApp(App);
